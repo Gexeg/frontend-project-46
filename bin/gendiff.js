1bcab2f3b8bda2d5713aca0a9de836098902bc80
Aleksandr <psiondnd@yandex.ru>
2024-10-03 16:58:57 +0500

    added format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,9 +10,11 @@ program
   .name('gendiff')
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
+  .requiredOption('-f, --format <type>', 'Output format');
 
 program
     .command('gendiff')
+    .argument('<format>', 'user to login')
     .action(command);
 
 program.parse();
